0019d037f2f52a5cad9ce588fee1486d2305cca7
d-saraki <abiola_saraki@live.co.uk>
2024-02-11 20:44:33 +0000

    update Admin config file

--- bloodtests/admin.py
+++ bloodtests/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Test, TestResult
+
+from bloodtests.models import Test, TestResult
+
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "unit", "upper", "lower", "ideal_range")
+    search_fields = ("code", "name")
+    list_filter = ("unit",)
+    readonly_fields = ("ideal_range",)
+
 
 admin.site.register(Test)
-admin.site.register(TestResult)
+# admin.site.register(TestResult)
